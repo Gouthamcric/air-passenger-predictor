c58eb0e8eb158b1694f7ed35a41479b895fb7a54
Goutham N <goutham.n@vitap.ac.in>
2020-05-18 17:28:40 +0530

    update on 18th may 2020

---
+++ flask/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from keras.models import load_model
+import numpy as np
+global model,graph
+import tensorflow as tf
+
+# load the pre-trained Keras model
+model=load_model("models/passenger_predict.h5")
+graph = tf.get_default_graph()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ind')
+def index2():
+    return render_template('index2.html')
+
+@app.route('/predict',methods= ['POST'])
+def predict():
+    global graph
+    with graph.as_default():
+     p=request.get_data()
+     from sklearn.preprocessing import MinMaxScaler
+     import pandas as pd
+     sc = MinMaxScaler()
+     data=pd.read_csv("../final_dataset/air_passenger.csv")
+     dataset_train=data
+     train_set=dataset_train.iloc[:,1:2]
+     train_set=train_set.append({'#Passengers':p},ignore_index=True)
+     scaled_training=sc.fit_transform(train_set)
+     y_predict = model.predict([[np.array([[scaled_training[144][0]]])]])
+     y_pred=sc.inverse_transform(y_predict)[0][0]
+    return str(y_pred)
+
+if __name__ == '__main__':
+    app.run(debug=True)
